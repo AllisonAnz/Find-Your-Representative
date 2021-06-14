68e619e6d26ef7fc5366452602210314b52a1824
AllisonAnz <AllisonAnzalone@gmail.com>
2021-06-14 12:11:15 -0500

    index.js updated

--- src/index.js
+++ src/index.js
@@ -28,16 +28,6 @@ const init = () => {
     })
 }
 
-
-function electionUrl() {
-    fetch(`${BASE_URL}/elections?&key=${API_KEY}`)
-        .then(response => response.json())
-        .then(data =>    
-            renderElectionData(data))
-        .catch(error => console.log('error', error));
-}
-
-
 function repUrl(address) {
     fetch(`${BASE_URL}/${parameterName}?address=${address}&key=${API_KEY}`)
         .then(response => response.json())
@@ -46,9 +36,71 @@ function repUrl(address) {
         .catch(error => console.log('error', error))
 }
 
+function electionUrl() {
+    fetch(`${BASE_URL}/elections?&key=${API_KEY}`)
+        .then(response => response.json())
+        .then(data =>
+            renderElectionData(data))
+        .catch(error => console.log('error', error));
+}
+
+
+function renderOfficials(data) {
+
+    let offices = data['offices']
+    let officials = data['officials']
+
+    offices.forEach(office => {
+        let officeIndex = office.officialIndices
+        officeNames = office.name
+
+        officeIndex.forEach(idx => {
+            rep = officials[idx]
+        })
+        renderRepData(rep)
+    })
+
+    function renderRepData() {
+        let divCollect = document.querySelector('#collection')
+        let divCard = document.createElement('div')
+        divCard.setAttribute('class', 'card')
+
+        let h2 = document.createElement('h2')
+        h2.innerHTML = officeNames
+
+        let h3 = document.createElement('h3')
+        h3.innerHTML = rep.name
+
+        let h4 = document.createElement('h4')
+        h4.innerHTML = rep.phones
+
+        let h5 = document.createElement('h5')
+        h5.innerHTML = rep.party
+
+        let a = document.createElement('a')
+        a.setAttribute('href', rep.urls)
+        a.innerHTML = rep.urls
+
+        let img = document.createElement('img')
+        img.setAttribute('src', rep.photoUrl || 'https://thumbs.dreamstime.com/b/government-icon-vector-male-person-profile-avatar-building-symbol-political-governance-glyph-pictogram-144250791.jpg')
+        img.setAttribute("width", '50px')
+        img.setAttribute('class', 'center')
+        
+        divCard.append(h2, img, h3, h4, h5, a)
+        divCollect.append(divCard)
+    }
+
+    let divTitle = document.querySelector('#title')
+    let p = document.createElement('p')
+    p.innerHTML = "Representatives"
+    p.setAttribute('id', "title-form")
+    divTitle.append(p)
+
+}
+
 function renderElectionData(data) {
     let electionInfo = data['elections']
-    
+
     let table = document.querySelector("table");
     let thead = document.createElement("thead")
     let th1 = document.createElement("th")
@@ -72,60 +124,6 @@ function renderElectionData(data) {
     }
 }
 
-
-function renderOfficials(data) {
-
-    let offices = data['offices']
-    let officials = data['officials']
-
-    offices.forEach(office => {
-        let officeIndex = office.officialIndices
-        officeNames = office.name
-
-        officeIndex.forEach(idx => {
-            rep = officials[idx]
-        })
-        renderRepData(rep)
-    })
-
-    function renderRepData() {
-        let divCollect = document.querySelector('#collection')
-        let divCard = document.createElement('div')
-
-        let h2 = document.createElement('h2')
-        h2.innerHTML = officeNames
-
-        let h3 = document.createElement('h3')
-        h3.innerHTML = rep.name
-
-        let h4 = document.createElement('h4')
-        h4.innerHTML = rep.phones
-
-        let h5 = document.createElement('h5')
-        h5.innerHTML = rep.party
-
-        let a = document.createElement('a')
-        a.setAttribute('href', rep.urls)
-        a.innerHTML = rep.urls
-
-        let img = document.createElement('img')
-        img.setAttribute('src', rep.photoUrl || 'https://thumbs.dreamstime.com/b/government-icon-vector-male-person-profile-avatar-building-symbol-political-governance-glyph-pictogram-144250791.jpg')
-        img.setAttribute("width", '50px')
-        img.setAttribute('class', 'center')
-
-       
-        divCard.setAttribute('class', 'card')
-        divCard.append(h2, img, h3, h4, h5, a)
-        divCollect.append(divCard)
-    }
-
-    let divTitle = document.querySelector('#title')
-    let p = document.createElement('p')
-    p.innerHTML = "Representatives"
-    p.setAttribute('id', "title-form")
-    divTitle.append(p)
-
-}
 
 
 document.addEventListener('DOMContentLoaded', init)
